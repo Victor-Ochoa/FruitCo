2260f50fa23f770f02305b80f71200393e74373b
Victor Ochoa <Victor.Ochoa@POSSIBLE.com>
2020-12-23 16:34:06 -0300

    initial header feature

---
+++ src/Feature/Header/code/Controllers/HeaderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FruitCo.Feature.Header.Controllers
+{
+    public class HeaderController : Controller, IController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Feature/Header/code/Views/Header/readme.txt
@@ -0,0 +1,3 @@
+This file is just to include 'Views/Header' automatically in the project. 
+
+Feel free to delete.
